338e76a85cc0fdb2c784f443c1b37da3ec5e3852
David Camara <jdpcii@gmail.com>
2019-07-03 10:13:56 -0400

    Fix setup chef script

--- study/extchef/scripts/setupchef.sh
+++ study/extchef/scripts/setupchef.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo su -
-cd /tmp
-curl -O https://packages.chef.io/files/stable/chef-server/12.17.33/el/7/chef-server-core-12.17.33-1.el7.x86_64.rpm
-rpm -Uvh chef-server-core-12.17.33-1.el7.x86_64.rpm
-chef-server-ctl reconfigure
+sudo curl -O https://packages.chef.io/files/stable/chef-server/12.17.33/el/7/chef-server-core-12.17.33-1.el7.x86_64.rpm -o /tmp/chef-server-core-12.17.33-1.el7.x86_64.rpm
+sudo rpm -Uvh /tmp/chef-server-core-12.17.33-1.el7.x86_64.rpm
+sudo chef-server-ctl reconfigure
 read -p 'Press [Enter] to continue'
-chef-server-ctl user-create extchefuser ExtendChef User extchefuser@chef.io 'extchefuser1' --filename extchefuser.pem
-chef-server-ctl org-create extchefllc 'Extend Chef LLC' --association_user extchefuser --filename extchefllc-validator.pem
+sudo chef-server-ctl user-create extchefuser ExtendChef User extchefuser@chef.io 'extchefuser1' --filename /tmp/extchefuser.pem
+sudo chef-server-ctl org-create extchefllc 'Extend Chef LLC' --association_user extchefuser --filename /tmp/extchefllc-validator.pem
--- study/extchef/scripts/setupchefdk.sh
+++ study/extchef/scripts/setupchefdk.sh
@@ -2,7 +2,7 @@
 cd /tmp
 curl -O https://packages.chef.io/files/stable/chefdk/2.5.13/el/7/chefdk-2.5.13-1.el7.x86_64.rpm
 sudo rpm -Uvh chefdk-2.5.13-1.el7.x86_64.rpm
-echo 'eval "$(chef shell-init bash)"' >> ~/.b
+echo 'eval "$(chef shell-init bash)"' >> ~/.bash_profile
 source ~/.bash_profile
 cd ~
 chef generate repo extchefrepo
