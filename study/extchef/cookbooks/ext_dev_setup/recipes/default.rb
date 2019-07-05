4c9ad2b794d33201e0edd69912c8b567e620411d
temp name <temp@tempme.info>
2019-07-05 19:26:19 +0000

    failed trying to call child resource from parent

--- study/extchef/cookbooks/ext_dev_setup/recipes/default.rb
+++ study/extchef/cookbooks/ext_dev_setup/recipes/default.rb
@@ -6,3 +6,13 @@
 package 'epel-release'
 
 ext_dev_setup_init 'www.example.com'
+
+execute 'from parent recipe' do
+  command 'echo "from parent recipe"'
+  action :nothing
+end
+
+execute 'run child resource' do
+  command 'echo "Try to run child resource"'
+  notifies :run, 'execute[echo from custom]', :immediately
+end
--- study/extchef/cookbooks/ext_dev_setup/resources/init.rb
+++ study/extchef/cookbooks/ext_dev_setup/resources/init.rb
@@ -3,6 +3,16 @@ property :host, String, name_property: true
 property :content, String
 
 action :create do
+  execute 'echo from init resource' do
+    command 'echo "from same init resource"'
+    action :nothing
+  end
+
+  execute 'echo from custom' do
+    command 'echo "from child init"'
+    action :nothing
+  end
+
   nginx_server new_resource.host do
     if new_resource.content
       landing_page_content new_resource.content
@@ -11,10 +21,12 @@ action :create do
 
   directory '/etc/chef/ohai/hints' do
     recursive true
+    notifies :run, 'execute[echo from init resource]', :immediately
   end
 
   json_config "/etc/chef/ohai/hints/#{new_resource.host}.json" do
     content({ host: new_resource.host })
+    notifies :run, 'execute[from parent recipe]', :immediately
   end
 end
 
