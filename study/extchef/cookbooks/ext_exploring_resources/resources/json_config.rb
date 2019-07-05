363ee9a9ec6cacb5e5fadb95fc10c0f1bb63a237
temp name <temp@tempme.info>
2019-07-05 16:02:01 +0000

    Idempotency example

--- study/extchef/cookbooks/ext_exploring_resources/resources/json_config.rb
+++ study/extchef/cookbooks/ext_exploring_resources/resources/json_config.rb
@@ -2,24 +2,45 @@
 resource_name :json_config
 
 property :path, String, name_property: true
-property :content, [Hash, String], required: true, callbacks: {
+property :content, [Hash, String], callbacks: {
   'check valid JSON' => lambda { |content|
-     return true if content.is_a?(Hash)
-     begin
-       JSON.parse(content)
-     rescue
-       false
-     end
+     content.is_a?(Mash)
   }
+}, coerce: lambda { |content|
+   return Mash.new(content) if content.is_a?(Hash)
+   begin
+     Mash.new(JSON.parse(content))
+   rescue
+     content
+   end
 }
 
 default_action :create
 
+load_current_value do
+  if ::File.exists?(path)
+    myjson = ::File.open(path, 'r')
+    content JSON.parse(myjson.read)
+  end
+end
+
 action :delete do
-  puts "\n\n Removing the config\n\n"
+  if ::File.exists?(new_resource.path)
+    converge_by "Removing the config #{new_resource.path}" do
+      ::FileUtils.rm(new_resource.path)
+    end
+  end
 end
 
 action :create do
-  puts "\n\n Setting up the config with #{new_resource.content} \n\n"
+  converge_if_changed do
+    puts "Setting up the config with #{new_resource.content}"
+    file = ::File.open(new_resource.path, 'w')
+    if new_resource.content.is_a?(String)
+      file.write(new_resource.content)
+    else
+      file.write(new_resource.content.to_json)
+    end
+  end
 end
 
