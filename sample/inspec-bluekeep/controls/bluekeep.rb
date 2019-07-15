5be87a61bddecbce11a658c68886e37bbd5a4cc6
David Camara <jdpcii@gmail.com>
2019-07-15 14:48:44 -0400

    Testing for KB install for older OS

--- sample/inspec-bluekeep/controls/bluekeep.rb
+++ sample/inspec-bluekeep/controls/bluekeep.rb
@@ -9,16 +9,37 @@ control 'bluekeep-profile' do
   desc 'Confirm node is protected from the bluekeep vulnerability'
 
   if os.windows?
-    describe os.release.split('.')[2].to_i do
-      it { should be >= 9200 }
-    end
+    case os.release.split('.')[2].to_i
+    when 6003
+      script = <<-EOH
+        (Get-HotFix -Id "KB4499180") -or (Get-HotFix -Id "KB4499149")
+      EOH
+  
+      describe powershell(script) do
+        its('stdout') { should eq "True\r\n" }
+        its('stderr') { should eq '' }
+      end
+    when 7601
+      script = <<-EOH
+        (Get-HotFix -Id "KB4499175") -or (Get-HotFix -Id "KB4499164")
+      EOH
+  
+      describe powershell(script) do
+        its('stdout') { should eq "True\r\n" }
+        its('stderr') { should eq '' }
+      end
+    else
+      describe os.release.split('.')[2].to_i do
+        it { should be >= 9200 }
+      end
 
-    describe registry_key('HKLM\System\CurrentControlSet\Control\Terminal Server') do
-      its('fDenyTSConnections') { should eq 0x1 }
-    end
+      describe registry_key('HKLM\System\CurrentControlSet\Control\Terminal Server') do
+        its('fDenyTSConnections') { should eq 0x1 }
+      end
     
-    describe registry_key('HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp') do
-      its('UserAuthentication') { should eq 0x1 }
+      describe registry_key('HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp') do
+        its('UserAuthentication') { should eq 0x1 }
+      end
     end
   end
 end
